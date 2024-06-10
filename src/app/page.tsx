8c1ffb777f523c270bff92d33716eaa6d0017b0f
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-06-10 13:37:54 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,12 @@ function Navbar() {
   return (
     <div className='w-full text-white h-16 backdrop-filter backdrop-blur-xl bg-opacity-20 flex items-center justify-center'>
       <div className='max-w-7xl w-full flex items-center justify-between p-4'>
-        <h6 className='font-bold hover:text-fuchsia-300 transition-colors text-lg'>nextbot</h6>
+        <h6 className='font-bold hover:text-fuchsia-300 transition-colors text-s'>nextbot</h6>
         <ul className='flex gap-8'>
-          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#home">Home</Link></li>
-          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#about">About</Link></li>
-          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#pricing">Pricing</Link></li>
-          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#pricing">Contact Us</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xs sm:text-base' href="#home">Home</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xs sm:text-base' href="#about">About</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xs sm:text-base' href="#pricing">Pricing</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xs sm:text-base' href="#pricing">Contact Us</Link></li>
         </ul>
       </div>
     </div>
