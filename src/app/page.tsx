d521f805a2ac4a7fe1e804b904337ee2abacba13
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-06-10 13:59:06 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ function Navbar() {
   return (
     <div className='w-full text-white h-16 backdrop-filter backdrop-blur-xl bg-opacity-20 flex items-center justify-center'>
       <div className='max-w-7xl w-full flex items-center justify-between p-4'>
-        <h6 className='font-bold hover:text-fuchsia-300 transition-colors text-s'>nextbot</h6>
+        <h6 className='font-bold hover:text-fuchsia-300 transition-colors text-xs'>nextbot</h6>
         <ul className='flex gap-8'>
           <li><Link className='hover:text-fuchsia-300 transition-colors text-xs sm:text-base' href="#home">Home</Link></li>
           <li><Link className='hover:text-fuchsia-300 transition-colors text-xs sm:text-base' href="#about">About</Link></li>
