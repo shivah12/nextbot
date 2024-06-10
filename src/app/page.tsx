132ebd8be02b2978b6c9df07225e6e9c827a91f2
Shreeti Mohapatra <114981873+shivah12@users.noreply.github.com>
2024-06-10 13:26:12 +0530

    Updated navar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
 import Spline from '@splinetool/react-spline/next';
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      {/* Navbar */}
-      <nav className="bg-black py-4 backdrop-filter backdrop-blur-md">
-        <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
-          <div>
-            <span className="text-white text-lg font-bold cursor-pointer hover:text-fuchsia-300 transition duration-300">nextbot</span>
-          </div>
-          <div className="flex items-center">
-            <ul className="flex space-x-8 text-white">
-              <li className="cursor-pointer hover:text-fuchsia-300 transition duration-300">Home</li>
-              <li className="cursor-pointer hover:text-fuchsia-300 transition duration-300">About</li>
-              <li className="cursor-pointer hover:text-fuchsia-300 transition duration-300">Services</li>
-              <li className="cursor-pointer hover:text-fuchsia-300 transition duration-300">Contact</li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
       {/* Main content */}
       <main className="bg-black">
+      <Navbar />
         {/* Increased size of the section */}
         <section className="h-screen flex justify-center items-center bg-black text-white">
           <Spline
@@ -31,4 +15,20 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+function Navbar() {
+  return (
+    <div className='w-full text-white h-16 backdrop-filter backdrop-blur-xl bg-opacity-20 flex items-center justify-center'>
+      <div className='max-w-7xl w-full flex items-center justify-between p-4'>
+        <h6 className='font-bold hover:text-fuchsia-300 transition-colors text-lg'>nextbot</h6>
+        <ul className='flex gap-8'>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#home">Home</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#about">About</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#pricing">Pricing</Link></li>
+          <li><Link className='hover:text-fuchsia-300 transition-colors text-xxs sm:text-base' href="#pricing">Contact Us</Link></li>
+        </ul>
+      </div>
+    </div>
+  )
 }
